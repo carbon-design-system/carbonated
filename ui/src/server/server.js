0088d1d076a4e16f50a98f30f291cb5c6bec943f
Josh Black <josh@josh.black>
2019-02-02 14:00:27 -0600

    feat(ci): add ci workflow

--- ui/src/server/server.js
+++ ui/src/server/server.js
@@ -30,7 +30,7 @@ const middleware = [
   https,
 
   // Development Middleware for handling client-side related development
-  setupDevelopment(getConfig),
+  // setupDevelopment(getConfig),
 
   // Middleware that should be enabled for most requests
   all,
@@ -38,54 +38,49 @@ const middleware = [
   // "Security" middleware
   security,
 
-  // Setup handling of different languages/locales
-  setupLocale([...supported]),
-
-  // Handle serving static assets provided through ASSET_PATH
-  assets,
   server => {
-    server.use(
-      '/static',
-      express.static(path.resolve(__dirname, '../../public'))
-    );
+    server.use((req, res) => {
+      res.send('OK');
+    });
     return server;
   },
 
-  // Handle generating HTML responses, serving static assets, and error handling
-  setupHTML({
-    getTitle: () => 'Page Title',
-    getMetaTags: () => ({
-      og: {
-        title: 'Page Title',
-        type: 'website',
-        url: 'https://google.com',
-        description: 'Page description',
-      },
-    }),
-    getMessages,
-  }),
+  // Setup handling of different languages/locales
+  // setupLocale([...supported]),
+
+  // // Handle serving static assets provided through ASSET_PATH
+  // assets,
+  // server => {
+  // server.use(
+  // '/static',
+  // express.static(path.resolve(__dirname, '../../public'))
+  // );
+  // return server;
+  // },
+
+  // // Handle generating HTML responses, serving static assets, and error handling
+  // setupHTML({
+  // getTitle: () => 'Page Title',
+  // getMetaTags: () => ({
+  // og: {
+  // title: 'Page Title',
+  // type: 'website',
+  // url: 'https://google.com',
+  // description: 'Page description',
+  // },
+  // }),
+  // getMessages,
+  // }),
 
   // Error handling so we don't pollute the response with stack traces
   error,
 ].filter(Boolean);
 const ASSET_PATH = path.resolve(__dirname, '../../build');
 const context = {
-  build: getBuildContext({
-    assetPath: ASSET_PATH,
-    getConfig,
-  }),
+  // build: getBuildContext({
+  // assetPath: ASSET_PATH,
+  // getConfig,
+  // }),
 };
 
-module.exports = () =>
-  applyMiddleware(
-    server,
-    [
-      server => {
-        server.use((req, res) => {
-          res.send('OK');
-        });
-        return server;
-      },
-    ],
-    context
-  );
+module.exports = () => applyMiddleware(server, middleware, context);
