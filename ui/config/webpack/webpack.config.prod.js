e8b5f9a0836d2f99333ad164288e962af2d16935
Josh Black <josh@josh.black>
2019-02-02 19:41:47 -0600

    fix(ui): remove invalid option from css-loader

--- ui/config/webpack/webpack.config.prod.js
+++ ui/config/webpack/webpack.config.prod.js
@@ -158,8 +158,7 @@ module.exports = {
               {
                 loader: require.resolve('css-loader'),
                 options: {
-                  importLoaders: 1,
-                  minimize: true,
+                  importLoaders: 3,
                   sourceMap: true,
                 },
               },
