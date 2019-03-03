494f1590a7ea8f536dd5d6139beed4c2eaeb9662
Josh Black <josh@josh.black>
2019-03-03 17:57:25 -0600

    chore(ui): add copyright banner

--- ui/src/shared/languages/index.js
+++ ui/src/shared/languages/index.js
@@ -1,5 +1,13 @@
-const supported = new Set(['en', 'en-US']);
+/**
+ * Copyright IBM Corp. 2019, 2019
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
+'use strict';
+
+const supported = new Set(['en', 'en-US']);
 const getLanguageFromLocale = locale => locale.split('-')[0];
 
 const languages = [...supported]
